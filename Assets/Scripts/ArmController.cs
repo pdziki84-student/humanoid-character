a8c8d28ab635667006ea297ccde9e510740ce079
pdziki84-student <wasza.dzikosc@gmail.com>
2021-02-04 20:14:46 +0100

    humaniod-character

---
+++ Assets/Scripts/ArmController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    bool armWaving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetComponent<Animator>().SetBool("reka", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            armWaving = !armWaving;
+        }
+        
+        if (armWaving) 
+        {
+            transform.GetComponent<Animator>().SetBool("reka", true);
+        }
+        else
+        {
+            transform.GetComponent<Animator>().SetBool("reka", false);
+        }
+    }
+}
